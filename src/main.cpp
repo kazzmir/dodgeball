2d0df70f75a9fa28f306bd68bfe00d1cb8f8820f
Jon Rafkind <jon@rafkind.com>
2012-12-23 03:29:56 +0000

    more basic setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,40 @@
 #include "util/init.h"
+#include "util/events.h"
+#include "util/input/input-manager.h"
+#include "util/debug.h"
+#include "util/exceptions/exception.h"
+#include "util/exceptions/shutdown_exception.h"
 
-int main(){
+static void run(){
+    class Main: public Util::Logic, public Util::Draw {
+    public:
+        void draw(const Graphics::Bitmap & screen){
+        }
+
+        void run(){
+        }
+
+        bool done(){
+            return false;
+        }
+
+        double ticks(double time){
+            return time;
+        }
+    };
+
+    Main main;
+    Util::standardLoop(main, main);
+}
+
+int main(int argc, char ** argv){
     Global::init(Global::WINDOWED);
+    InputManager input;
+    try{
+        run();
+    } catch (const ShutdownException & fail){
+        Global::debug(0) << "Shutdown" << std::endl;
+    }
+
+    Global::close();
 }
